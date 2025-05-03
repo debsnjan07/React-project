7a188036e9ff6598534cee1b1cdc3355df88c4ea
Debanjan Acharjee <debanjanacharjee07@gmail.com>
2025-05-03 21:09:46 +0530

    Third commit

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,12 +1,37 @@
-
+import { useState } from "react";
 
 function App() {
+
+  let [counter,setCounter] = useState(15);
+  let [decreaser,setDecreaser] = useState(15);
+
+  const addvalue = () =>{
+    if(counter<20){
+    counter = counter+1
+    }
+    setCounter(counter)
+
+  }
+  const decrease = () =>{
+    if(decreaser>0){
+    decreaser = decreaser-1
+    }
+    setDecreaser(decreaser)
+
+  }
+ 
  
 
   return (
    <>
    <h1>Chai aur react using vite </h1>
-   <img src="assets/react.svg"></img>
+   <button onClick={addvalue}
+   >Add value</button>
+   <button onClick={decrease}
+   >Decrease value</button>
+   <h2>Addvalue{counter}</h2>
+   <h2>Decreasevalue{decreaser}</h2>
+
    </>
   )
 }
